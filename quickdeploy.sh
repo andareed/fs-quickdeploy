3bde0117cee6519d75fcd3fb4c903a76d1c6f245
andareed <andrew.reed@forescout.com>
2025-06-20 11:47:31 +0100

    Added another sh file

---
+++ quickdeploy.sh
@@ -0,0 +1,5 @@
+# Quick Deploy - a method of deploying Eyesight settings quickly and efficiently.
+# Author: A Reed (2025)
+# To include a pub key into the authorized file as part of the deployment see the readme.
+#
+
